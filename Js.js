a9711d669022c088ba4a96d50a3f2cf0af90edfa
brennoprettyman <brennoprettyman@gmail.com>
2023-10-26 19:40:33 -0300

    Terminado

---
+++ Js.js
@@ -0,0 +1,20 @@
+const nome = prompt ("Digite seu 1º Nome: ");
+const sobrenome = prompt ("Digite seu Sobrenome: ");
+const nomecompleto = `${nome} ${sobrenome}`
+
+alert(`Nome completo: ${nomecompleto}`);
+alert(`Nome de Catálogo: ${sobrenome.toUpperCase()}, ${nome}.`)
+
+
+
+
+
+/*string nome, sobrenome;
+Console.Write("Digite seu nome.......: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome..: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");*/
